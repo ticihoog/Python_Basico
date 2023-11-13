30f2ebf6058962267131c2782afa4319a32644a4
ticihoog <ticianapaternostro@gmail.com>
2023-11-12 23:51:06 -0300

    alterações PP-P001

--- Atividades/PP-P01/Exc_03.py
+++
@@ -1,6 +0,0 @@
-# Parte 1: Imprimir caracteres numéricos e seus códigos numéricos
-print("Parte 1:")
-for i in range(10):
-    char = chr(48 + i)
-    code = ord(char)
-    print(f"'{char}' - {code}")
